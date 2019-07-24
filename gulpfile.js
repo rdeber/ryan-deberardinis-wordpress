e9ed581b75ba6b926d1794a74e144d2f3c2702e9
Ryan DeBerardinis <rdeber@gmail.com>
2019-07-24 14:23:10 -0400

    Gulp process integration.

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var input = './styles/main.scss';
+var output = './public/css';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
+});
